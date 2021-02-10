2e1f0c4ea0b486c7ccef7bc3e0edbbf35dbd0250
Nathan-E-White <nwnn4@umsystem.edu>
2021-02-10 15:30:33 -0600

    Added Get statements for URL builder files to init.m.

--- YahooFinanceTools/Kernel/init.m
+++ YahooFinanceTools/Kernel/init.m
@@ -1,5 +1,24 @@
 (* Mathematica Init file *)
 (* Created by the Wolfram Language Plugin for IntelliJ, see http://wlplugin.halirutan.de/ *)
 
+(* MAIN YAHOO FINANCE PACKAGE *)
 Get["YahooFinanceTools`YahooFinanceTools`"];
 
+(* URL BUILDERS *)
+Get["YahooFinanceTools`BuildYFSymbolSustainabilityURL`"];
+Get["YahooFinanceTools`BuildYFSymbolSummaryURL`"];
+Get["YahooFinanceTools`BuildYFSymbolStatisticsURL`"];
+Get["YahooFinanceTools`BuildYFSymbolSearchURL`"];
+Get["YahooFinanceTools`BuildYFSymbolProfileURL`"];
+Get["YahooFinanceTools`BuildYFSymbolOutlookURL`"];
+Get["YahooFinanceTools`BuildYFSymbolOptionsURL`"];
+Get["YahooFinanceTools`BuildYFSymbolAnalysisURL`"];
+Get["YahooFinanceTools`BuildYFSymbolBalanceSheetURL`"];
+Get["YahooFinanceTools`BuildYFSymbolCashFlowURL`"];
+Get["YahooFinanceTools`BuildYFSymbolChatterURL`"];
+Get["YahooFinanceTools`BuildYFSymbolHistoryURL`"];
+Get["YahooFinanceTools`BuildYFSymbolIncomeStatementURL`"];
+Get["YahooFinanceTools`BuildYFSymbolInsiderRosterURL`"];
+Get["YahooFinanceTools`BuildYFSymbolInsiderTradesURL`"];
+Get["YahooFinanceTools`BuildYFSymbolInstitutionalHoldersURL`"];
+Get["YahooFinanceTools`BuildYFDownloadHistoricalDataURL`"];
